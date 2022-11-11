a15672e921e0015c7fe9f0534c1cbb1ef6c1fbe0
kypocaku <mrcrash0811@gmail.com>
2022-11-11 21:40:18 +0300

    Изменения для класса GenerateString по замечанию преподавателя

---
+++ app/src/main/java/com/example/laba_dimas/GenerateString.kt
@@ -0,0 +1,41 @@
+package com.example.laba_1
+
+class GenerateString(var n : Int) {
+    private var listBig : MutableList<String> = mutableListOf()
+    private var listLittle : MutableList<String> = mutableListOf()
+
+    private var text : String = "1 111 222 3333 44444 555555 4854054904 1 53 53ш53 ооыолпып"
+    var textSeparated = text.split(" ").toTypedArray() // разбиваем строку на слова
+
+    fun sort(length : Int){ // разместил по массивам
+        for(i in textSeparated.indices){
+            if(textSeparated[i].length>=length){
+                listBig.add(textSeparated[i])
+            }
+            else{
+                listLittle.add(textSeparated[i])
+            }
+        }
+    }
+
+    fun getLittle() : MutableList<String>{
+        return listLittle
+    }
+
+    fun getBig() : MutableList<String>{
+            return  listBig
+    }
+
+    fun getText() : String{
+        return text
+    }
+
+    fun clearStringSorted(){
+        listBig= mutableListOf()
+        listLittle= mutableListOf()
+    }
+
+
+
+
+}
